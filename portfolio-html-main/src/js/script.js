894c70592a90110e38078f46b1303ac2008b4efb
Mr_je___ <108830016+mrjaysojitra@users.noreply.github.com>
2023-11-30 22:09:42 +0530

    update

--- portfolio-html-main/src/js/script.js
+++ portfolio-html-main/src/js/script.js
@@ -25,9 +25,9 @@ const toggleSwitch = document.querySelector(
 
 function switchTheme(e) {
   if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
-  } else {
     document.documentElement.setAttribute("data-theme", "light");
+  } else {
+    document.documentElement.setAttribute("data-theme", "dark");
   }
 }
 
@@ -37,11 +37,11 @@ toggleSwitch.addEventListener("change", switchTheme, false);
 
 function switchTheme(e) {
   if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "light");
-    localStorage.setItem("theme", "light"); //add this
-  } else {
     document.documentElement.setAttribute("data-theme", "dark");
     localStorage.setItem("theme", "dark"); //add this
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light"); //add this
   }
 }
 
